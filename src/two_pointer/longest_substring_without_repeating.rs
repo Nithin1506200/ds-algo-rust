51e81cfab5d1d86b15366ecaed94d5601bfeb7ea
nithin <nithingowdan77@gmail.com>
2025-01-30 12:25:56 +0530

    update

--- src/two_pointer/longest_substring_without_repeating.rs
+++ src/two_pointer/longest_substring_without_repeating.rs
@@ -1,6 +1,24 @@
 use std::collections::HashMap;
+//Longest Substring Without Repeating Characters
 
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 pub fn length_of_longest_substring(s: String) -> i32 {
     let mut ans: i32 = 0;
     let mut hp: HashMap<char, i32> = HashMap::new();
@@ -19,5 +37,9 @@ pub fn length_of_longest_substring(s: String) -> i32 {
 mod test {
     use super::*;
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(length_of_longest_substring(String::from("abcabcbb")), 3);
+        assert_eq!(length_of_longest_substring(String::from("bbbbb")), 1);
+        assert_eq!(length_of_longest_substring(String::from("pwwkew")), 3);
+    }
 }
